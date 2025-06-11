6c1b5a2e93d219f591730a25ef6b6c67cfb357ff
patty-chow <pattychowlomeli@gmail.com>
2025-06-11 13:50:36 -0700

    Update trace logs, modify app paths manifest, and refresh webpack cache files for improved build performance and error handling.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "365472225cedcd79"; }
+/******/ 		__webpack_require__.h = function() { return "a2fbd563eb2298a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
